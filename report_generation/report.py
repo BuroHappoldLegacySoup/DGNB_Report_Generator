f492dfd7e638d23e7a5bb2024ebf1a0335d37ceb
Kartheek Mylavarapu <kartheek.mylavarapu@burohappold.com>
2024-06-14 17:23:15 +0200

    possible features in report

--- report_generation/report.py
+++ report_generation/report.py
@@ -3,3 +3,21 @@ class Report:
     def __init__(self,projectData):
         self.sections = []
         
+    def add_section(self):
+        return None
+
+    def add_header_and_footer(self):
+        return None
+
+    def add_text(self):
+        return None
+    
+    def add_bullet_list(self):
+        return None
+
+    def add_image(self):
+        return None
+
+    def add_table(self):
+        return None
+    
