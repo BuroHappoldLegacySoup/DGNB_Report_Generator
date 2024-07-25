4711ce601d6e7ad70b9c853ba4e1cde75eb19d6f
Erida Bendo <erida.bendo@burohappold.com>
2024-07-25 16:13:39 +0200

    remove UI & add file path from Excel File
    remove UI & add file path from Excel File

--- gui/gui.py
+++ gui/gui.py
@@ -1,8 +1,9 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QFileDialog, QLabel, QLineEdit, QMessageBox
+from pathlib import Path
 import os
 import glob
 import sys
-
+import argparse
 # Insert the parent directory into the system path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -11,51 +12,44 @@ from report_generation.interaction import Interaction
 from report_generation.docx_editor import DocxEditor
 
 class MyApp(QWidget):
-    def __init__(self):
+    def __init__(self, file_path):
         super().__init__()
-        # Initialize the UI
-        self.initUI()
-        # Resize the window
-        self.resize(500, 100)
-        # Initialize the folder path
-        self.folder_path = None
+        self.excel_path=file_path
+        self.folder_path=Path(file_path).parent
+        self.generate_report(self.excel_path)
+        
 
     def initUI(self):
-        # Create and set up the UI elements
-        outputFileNameLabel = QLabel('Output File Name: (optional)', self)
-        self.outputFileNameEdit = QLineEdit(self)
-        loadFolderBtn = QPushButton('Load Folder', self)
-        loadFolderBtn.clicked.connect(self.load_folder)
-        generateReportBtn = QPushButton('Generate report', self)
-        generateReportBtn.clicked.connect(self.generate_report)
-        vbox = QVBoxLayout()
-        vbox.addWidget(outputFileNameLabel)
-        vbox.addWidget(self.outputFileNameEdit)
-        vbox.addWidget(loadFolderBtn)
-        vbox.addWidget(generateReportBtn)
-        self.setLayout(vbox)
-        self.setWindowTitle('DGNB Report Generator')
-        self.show()
+        # Create a QVBoxLayout instance
+        layout = QVBoxLayout()
 
-    def load_folder(self):
-        # Open a dialog to select a folder
-        self.folder_path = QFileDialog.getExistingDirectory(self, 'Select Folder')
-        # Check the structure of the selected folder
-        self._check_folder_structure(self.folder_path)
+        # Create a QLabel widget and add it to the layout
+        label = QLabel(f"Generating Report from: {self.excel_path}", self)
+        layout.addWidget(label)
+
+        # Set the layout to the QWidget
+        self.setLayout(layout)
+
+        # Set the window title and size
+        self.setWindowTitle('DGNB Report Generator')
+        self.resize(300, 100)
+
 
     def _check_folder_structure(self, folder_path):
         # Check if the folder contains the necessary files
         files = os.listdir(folder_path)
-        if any(file.endswith('.xlsx') for file in files) and any(file.endswith('.docx') for file in files):
+        if any(file.endswith('.xlsm') for file in files) and any(file.endswith('.docx') for file in files):
             pass
         else:
             # Show an error message if the folder structure is not correct
             QMessageBox.critical(self, 'Error', 'Sorry folder structure doesn\'t comply with requirements.😞')
 
-    def generate_report(self):
+    def generate_report(self, excel_path):
         # Generate a report based on the files in the selected folder
+        self.folder_path = os.path.dirname(self.excel_path)
+        self._check_folder_structure(self.folder_path)
         docx_file = glob.glob(os.path.join(self.folder_path, '*.docx'))[0]
-        xlsx_file = glob.glob(os.path.join(self.folder_path, '*.xlsx'))[0]
+        xlsx_file = self.excel_path
         data = Interaction(xlsx_file)
         report = DocxEditor(docx_file)
         sheet_name = "SQ_Auditoreingaben "
@@ -63,10 +57,7 @@ class MyApp(QWidget):
         report.replace_key_words(data , sheet_name)
         images_folder = self.search_folder(self.folder_path, 'Images')
         report.replace_term_with_image("##Image##",images_folder)
-        if self.outputFileNameEdit.text() != "":
-            report.save_changes(self.outputFileNameEdit.text())
-        else:
-            report.save_changes("Output")
+        report.save_changes("Output")
 
         # Show a success message
         QMessageBox.information(self, "Success", "Report successfully generated!🥳\nPlease check the folder.")
@@ -81,6 +72,13 @@ class MyApp(QWidget):
 if __name__ == '__main__':
     # Start the application
     import sys
+
+    parser = argparse.ArgumentParser(description='Process an Excel file.')
+    parser.add_argument('file_path', type=str, help='Path to the Excel file')
+    args = parser.parse_args()
+
+
+
     app = QApplication(sys.argv)
-    ex = MyApp()
+    ex = MyApp(args.file_path)
     sys.exit(app.exec_())
